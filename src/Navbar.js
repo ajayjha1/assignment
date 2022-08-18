1bc03812752a3f7b4f274fa32259e1a1eebe4eeb
ajayjha1 <42446367+ajayjha1@users.noreply.github.com>
2022-08-18 15:18:51 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "/Users/ajayjha/my-app/src/images/logo.png";
+import logo from "./images/logo.png";
 import './navbar.css';
 const Navbar = () => {
     return(
